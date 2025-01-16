6affc6a4556d6fc2ab5eb5bf4c00826828dcde7b
Sourish Bhattacharjee <143819787+SourishBhattacherjee@users.noreply.github.com>
2025-01-16 21:18:07 +0530

    Create 56mergesubintervals.cpp
    Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
    
     
    
    Example 1:
    
    Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
    Output: [[1,6],[8,10],[15,18]]
    Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
    Example 2:
    
    Input: intervals = [[1,4],[4,5]]
    Output: [[1,5]]
    Explanation: Intervals [1,4] and [4,5] are considered overlapping.

---
+++ 56mergesubintervals.cpp
@@ -0,0 +1,44 @@
+//Optimal Solution
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end());
+        for (int i=0;i<intervals.size();i++){
+            if(ans.empty() || intervals[i][0]>ans.back()[1]){
+                ans.push_back(intervals[i]);
+            }
+            else{
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+            }
+        }
+        return ans;
+    }  
+}; 
+
+//Brute Force Solution
+/*class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end());
+        for(int i=0 ;i<intervals.size();i++){
+            int start=intervals[i][0];
+            int end=intervals[i][1];
+
+
+            if(!ans.empty() && intervals[i][1]<=ans.back()[1]){
+                continue;
+            }
+            for(int j=i+1;j<intervals.size();j++){
+                if(intervals[j][0]<=end){
+                    end=max(end,intervals[j][1]);
+                }else{
+                    break;
+                }
+            }
+            ans.push_back({start,end});
+        }
+        return ans;
+    }   
+};*/
